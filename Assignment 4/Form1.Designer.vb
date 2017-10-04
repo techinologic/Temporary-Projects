45cac2510b7100f730d98f955dbfb228e9ea6370
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-10-03 20:21:46 -0400

    final. working project with screenshot

--- Assignment 4/Form1.Designer.vb
+++ Assignment 4/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,65 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnDraw = New System.Windows.Forms.Button()
+        Me.lstResult = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnDraw
+        '
+        Me.btnDraw.Location = New System.Drawing.Point(96, 357)
+        Me.btnDraw.Name = "btnDraw"
+        Me.btnDraw.Size = New System.Drawing.Size(75, 23)
+        Me.btnDraw.TabIndex = 0
+        Me.btnDraw.Text = "Draw"
+        Me.btnDraw.UseVisualStyleBackColor = True
+        '
+        'lstResult
+        '
+        Me.lstResult.FormattingEnabled = True
+        Me.lstResult.Location = New System.Drawing.Point(13, 13)
+        Me.lstResult.Name = "lstResult"
+        Me.lstResult.Size = New System.Drawing.Size(243, 290)
+        Me.lstResult.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 325)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter number:"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(87, 322)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(122, 20)
+        Me.txtInput.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(268, 399)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstResult)
+        Me.Controls.Add(Me.btnDraw)
+        Me.Name = "Form1"
+        Me.Text = "Assignment 4"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnDraw As Button
+    Friend WithEvents lstResult As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtInput As TextBox
 End Class
--- Assignment 4/Form1.vb
+++ Assignment 4/Form1.vb
@@ -1,3 +1,55 @@
 ﻿Public Class Form1
 
+    Private Sub btnDraw_Click(sender As Object, e As EventArgs) Handles btnDraw.Click
+
+        Dim n As Integer = txtInput.Text
+        Dim print As String = ""
+
+        For i = 0 To n - 1
+            For j = 0 To n - 1
+                If j > i Then
+                    print = print & n - i
+                Else
+                    print = print & n - j
+                End If
+            Next
+
+            'top-right side
+            For j = n - 2 To 0 Step -1
+                If j < i Then
+                    print = print & n - j
+                Else
+                    print = print & n - i
+                End If
+            Next
+
+            lstResult.Items.Add(print)
+            print = "" 'clear the contents of print
+
+        Next
+
+        '2nd half
+        For i = n - 2 To 0 Step -1
+            For j = 0 To n
+                If j > i Then
+                    print = print & n - i
+                Else
+                    print = print & n - j
+                End If
+            Next
+
+            'bottom-right side
+            For j = n - 3 To 0 Step -1
+                If j < i Then
+                    print = print & n - j
+                Else
+                    print = print & n - i
+                End If
+            Next
+
+            lstResult.Items.Add(print)
+            print = ""
+        Next
+
+    End Sub
 End Class
